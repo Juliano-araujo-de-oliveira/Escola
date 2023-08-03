83e80efd87bae1f0980455c6d1c5f536073e7d3b
Juliano <julianoaraujo8749@gmail.com>
2023-08-03 19:12:18 -0300

    testarConexão

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React,{Component} from 'react';
 import {  Text, View, TouchableOpacity, Button } from 'react-native';
 
+import Itemdatabase from './src/components/dataBase/Itemdatabase';
 export default class App extends Component {
+
+
+testarConexao () {
+const db = new Itemdatabase();
+db.Conectar();
+db.Desconectar();
+}
+
+
+
+
+
 render(){
   return(
     <View>
+      <TouchableOpacity>
       <Text>Aprendendo SQLite</Text>
+      </TouchableOpacity>
     </View>
   )
 }
