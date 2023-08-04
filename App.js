95233b848deb5912a88ee7798fd27c8c3b44ce55
Juliano <julianoaraujo8749@gmail.com>
2023-08-03 23:35:49 -0300

    sqlite

--- App.js
+++ App.js
@@ -1,29 +1,56 @@
-import React,{Component} from 'react';
-import {  Text, View, TouchableOpacity, Button } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity, Button } from 'react-native';
 
+import Item from './src/components/models/Item';
 import Itemdatabase from './src/components/dataBase/Itemdatabase';
 import { Style } from './src/components/estilo/Style';
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+    nome:"item1",
+    idade:0,
+    turma:15,
+    endereco:"arroio do sal",
 
-testarConexao = () => {
-const db = new Itemdatabase();
-db.Conectar();
-db.Desconectar();
-}
+ 
+      lista: []
+    }
+  }
 
+  Listar = () => {
+    const db = new Itemdatabase();
+    db.Listar().then(
+      dados => {
+        this.setState({ lista: dados })
+      }
+    )
+  }
 
+  Cadastrar = (nome,idade,turma,endereco) => {
+    const db = new Itemdatabase();
+    const NovoItem = new Item(nome,idade,turma,endereco);
+    db.Cadastrar(NovoItem)
+  }
 
-
-
-render(){
-  return(
-    <View style={Style.container}>
-      <TouchableOpacity onPress={() => this.testarConexao()}>
-      <Text style={Style.escrita}>Aprendendo SQLite</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+  render() {
+    return (
+      <View style={Style.container}>
+        <TouchableOpacity onPress={()=> this.Cadastrar(this.state.nome,this.state.endereco,this.state.idade,this.state.turma)}>
+          <Text>Cadastrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.Listar()}>
+          <Text style={Style.escrita}>Aprendendo SQLite</Text>
+        </TouchableOpacity>
+       
+       {
+        this.state.lista.map(
+        item => (<Text>{item.id},{item.nome},{item.idade},{item.turma},{item.endereco}</Text>)
+        )
+       }
+      </View>
+    )
+  }
 }
 
--- src/components/dataBase/Itemdatabase.js
+++ src/components/dataBase/Itemdatabase.js
@@ -1,6 +1,7 @@
 //Importação do plugin SQLite
-import SQLite from "react-native-sqlite-storage"; 
-SQLite.DEBUG(true); 
+import SQLite from "react-native-sqlite-storage";
+import Item from "../models/Item";
+SQLite.DEBUG(true);
 SQLite.enablePromise(true);
 
 //Variáveis de conexão/criação do banco de dados 
@@ -10,53 +11,103 @@ const database_displayname = "SQLite React Offline Database"; //Nome de exibiç�
 const database_size = 200000; //tamanho do banco de dados
 
 //Função de inicialização do Banco de Dados
-export default class Database { 
-  Conectar() {  
+export default class Database {
+    Conectar() {
         let db;
-        return new Promise((resolve) => {    
-            console.log("Checando a integridade do plugin ...");    
-            SQLite.echoTest().then(() => {        
-                console.log("Integridade Ok ...");        
-                console.log("Abrindo Banco de Dados ...");        
+        return new Promise((resolve) => {
+            console.log("Checando a integridade do plugin ...");
+            SQLite.echoTest().then(() => {
+                console.log("Integridade Ok ...");
+                console.log("Abrindo Banco de Dados ...");
                 SQLite.openDatabase(database_name, database_version, database_displayname, database_size).then(DB => {
-                    db = DB;            
-                    console.log("Banco de dados Aberto");            
+                    db = DB;
+                    console.log("Banco de dados Aberto");
                     db.executeSql('SELECT 1 FROM Item LIMIT 1').then(() => {
                         console.log("O banco de dados está pronto ... Executando Consulta SQL ...");
-                    }).catch((error) =>{
+                    }).catch((error) => {
                         console.log("Erro Recebido: ", error);
                         console.log("O Banco de dados não está pronto ... Criando Dados");
                         db.transaction((tx) => {
-                            tx.executeSql('CREATE TABLE IF NOT EXISTS Produtos (id,nome,idade,turma,endereco)');
+                            tx.executeSql('CREATE TABLE IF NOT EXISTS Item (id INTEGER PRIMARY KEY AUTOINCREMENT,nome VARCHAR(50),idade INTERGER (20),turma INTERGER(20),endereco VARCHAR(50)');
                         }).then(() => {
-                            console.log("Tabela criada com Sucesso");                
-                        }).catch(error => {                    
-                            console.log(error);                
-                        });            
-                    });            
-                resolve(db);          
-            }).catch(error => {           
-                console.log(error);          
-            });      
-        }).catch(error => {        
-            console.log("echoTest Falhou - plugin não funcional");      
-        });    
-    }); 
-};
+                            console.log("Tabela criada com Sucesso");
+                        }).catch(error => {
+                            console.log(error);
+                        });
+                    });
+                    resolve(db);
+                }).catch(error => {
+                    console.log(error);
+                });
+            }).catch(error => {
+                console.log("echoTest Falhou - plugin não funcional");
+            });
+        });
+    };
 
- //Função para fechar a Conexão com o Banco de dados
-Desconectar(db) {  
-    if (db) {    
-        console.log("Fechando Banco de Dados");    
-        db.close().then(status => {        
-            console.log("Banco de dados Desconectado!!");      
-        }).catch(error => {        
-            this.errorCB(error);      
+    //Função para fechar a Conexão com o Banco de dados
+    Desconectar(db) {
+        if (db) {
+            console.log("Fechando Banco de Dados");
+            db.close().then(status => {
+                console.log("Banco de dados Desconectado!!");
+            }).catch(error => {
+                this.errorCB(error);
+            });
+        } else {
+            console.log("A conexão com o banco não está aberta");
+        }
+    };
+
+
+
+
+    //Função para listar itens da tabela produtos
+    Listar() {
+        return new Promise((resolve) => {
+            const lista = [];
+            this.Conectar().then((db) => {
+                db.transaction((tx) => {
+                    //Query SQL para listar os dados da tabela   
+                    tx.executeSql('SELECT * FROM Item;', []).then(([tx, results]) => {
+                        console.log("Consulta completa");
+                        let len = results.rows.length;
+                        for (let i = 0; i < len; i++) {
+                            let row = results.rows.item(i);
+                            const { id,nome,idade,turma,endereco } = row;
+                           lista.push({ id, nome,idade,turma,endereco });
+                        }
+                        console.log(lista);
+                        resolve(lista);
+                    });
+                }).then((result) => {
+                    this.Desconectar(db);
+                }).catch((err) => {
+                    console.log(err);
+                });
+            }).catch((err) => {
+                console.log(err);
+            });
+        });
+    }
+
+     // Função para acrescentar um novo produto na tabela
+     Cadastrar(item) {  
+        return new Promise((resolve) => {    
+            this.Conectar().then((db) => {      
+                db.transaction((tx) => {     
+                    //Query SQL para inserir um novo produto   
+                    tx.executeSql('INSERT INTO Item (nome,idade,turma,endereco ) VALUES (?, ?, ?, ?)', [item.id, item.nome, item.idade, item.turma, item.endereco]).then(([tx, results]) => { 
+                        resolve(results);        
+                    });      
+                }).then((result) => {        
+                    this.closeDatabase(db);      
+                }).catch((err) => {        
+                    console.log(err);      
+                });    
+            }).catch((err) => {      
+                console.log(err);    
+            });  
         });  
-    } else {    
-        console.log("A conexão com o banco não está aberta");  
-    } 
-};
-                        
-
+    }
 }
