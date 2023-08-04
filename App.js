d58ca1f4056588f065edae5b7ede41c32a80c4a4
Juliano <julianoaraujo8749@gmail.com>
2023-08-03 21:40:19 -0300

    estilização

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import {  Text, View, TouchableOpacity, Button } from 'react-native';
 
 import Itemdatabase from './src/components/dataBase/Itemdatabase';
+import { Style } from './src/components/estilo/Style';
 export default class App extends Component {
 
 
@@ -17,9 +18,9 @@ db.Desconectar();
 
 render(){
   return(
-    <View>
+    <View style={Style.container}>
       <TouchableOpacity onPress={() => this.testarConexao()}>
-      <Text>Aprendendo SQLite</Text>
+      <Text style={Style.escrita}>Aprendendo SQLite</Text>
       </TouchableOpacity>
     </View>
   )
---
+++ src/components/estilo/Style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const Style = StyleSheet.create({
+container:{
+    flex:1,
+    backgroundColor:'#008B8B',
+
+},
+escrita:{
+    fontSize:30,
+    textAlign:'center',
+    marginTop:50
+}
+
+})
+
+export {Style}
